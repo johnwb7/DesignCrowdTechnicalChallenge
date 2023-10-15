ce73659ed1ddb3711f403d76ec8bce3c296d8a73
jb <jwburkitt7@gmail.com>
2023-10-15 19:49:53 +1100

    Added implementation for WeekdaysBetweenTwoDates

--- DesignCrowdTechnicalChallenge/BusinessDayCounter.cs
+++ DesignCrowdTechnicalChallenge/BusinessDayCounter.cs
@@ -8,10 +8,26 @@ namespace DesignCrowdTechnicalChallenge
 {
     public class BusinessDayCounter
     {
+        private const int DaysInWeek = 7;
+        private const int WeekdaysInWeek = 5;
+
         public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            //todo
-            return -1;
+            if (secondDate <= firstDate) return 0;
+
+            var totalDays = (int)(secondDate - firstDate).TotalDays - 1; // -1 to exclude first date from result
+            var totalFullWeeks = totalDays / DaysInWeek;
+            var totalWeekdaysFromFullWeeks = totalFullWeeks * WeekdaysInWeek;
+
+            var firstDateDayOfWeek = GetClosestPriorWeekday(firstDate);
+            var secondDateDayOfWeek = GetClosestPriorWeekday(secondDate);
+            if (secondDateDayOfWeek <= firstDateDayOfWeek) secondDateDayOfWeek += WeekdaysInWeek;
+
+            var totalWeekdaysFromPartialWeek = IsWeekendDay(secondDate) ? 
+                secondDateDayOfWeek - firstDateDayOfWeek :
+                secondDateDayOfWeek - firstDateDayOfWeek - 1; // -1 to exclude second date from result
+            var totalWeekdays = totalWeekdaysFromFullWeeks + totalWeekdaysFromPartialWeek;
+            return totalWeekdays;
         }
         public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime>
         publicHolidays)
@@ -19,5 +35,9 @@ namespace DesignCrowdTechnicalChallenge
             //todo
             return -1;
         }
+
+        private int GetClosestPriorWeekday(DateTime dateTime) => IsWeekendDay(dateTime) ? (int)DayOfWeek.Friday : (int)dateTime.DayOfWeek;
+        private bool IsWeekendDay(DateTime dateTime) => dateTime.DayOfWeek == DayOfWeek.Saturday || dateTime.DayOfWeek == DayOfWeek.Sunday;
+
     }
 }
--- DesignCrowdTechnicalChallengeTests/BusinessDayCounterTests.cs
+++ DesignCrowdTechnicalChallengeTests/BusinessDayCounterTests.cs
@@ -8,11 +8,12 @@ namespace DesignCrowdTechnicalChallengeTests
     {
         private BusinessDayCounter _sut;
 
-        private static object[][] WeekdaysBetweenTwoDatesTestCases = {
-            new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1},
+        private static readonly object[][] WeekdaysBetweenTwoDatesTestCases = {
+            new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1}, // {firstDate, secondDate, expectedResult}
             new object[] { new DateTime(2013, 10, 5), new DateTime(2013, 10, 14), 5},
             new object[] { new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), 61},
-            new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 5), 0},
+            new object[] { new DateTime(2023, 09, 06), new DateTime(2023, 09, 14), 5},
+            new object[] { new DateTime(2023, 10, 12), new DateTime(2023, 10, 25), 8},
         };
 
         [SetUp]
@@ -22,13 +23,41 @@ namespace DesignCrowdTechnicalChallengeTests
         }
 
         [TestCaseSource(nameof(WeekdaysBetweenTwoDatesTestCases))]
-        public void WeekdaysBetweenTwoDates_CalculatesWeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, int expectedResult)
+        public void WeekdaysBetweenTwoDates_CalculatesNumberOfWeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, int expectedResult)
         {
             // Act
             var result = _sut.WeekdaysBetweenTwoDates(firstDate, secondDate);
 
             // Assert
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void WeekdaysBetweenTwoDates_WhenFirstDateAndSecondDateAreTheSameWeekday_CalculatesNumberOfWeekdaysbetweenTwoDates()
+        {
+            // Arrange
+            var firstDate = new DateTime(2023, 09, 14);
+            var secondDate = new DateTime(2023, 09, 28);
+
+            // Act
+            var result = _sut.WeekdaysBetweenTwoDates(firstDate, secondDate);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void WeekdaysBetweenTwoDates_SecondDateIsAWeekend_CalculatesNumberOfWeekdaysbetweenTwoDates()
+        {
+            // Arrange
+            var firstDate = new DateTime(2013, 1, 24);
+            var secondDate = new DateTime(2013, 2, 10);
+
+            // Act
+            var result = _sut.WeekdaysBetweenTwoDates(firstDate, secondDate);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(11));
         }
 
         [Test]
@@ -58,6 +87,7 @@ namespace DesignCrowdTechnicalChallengeTests
             Assert.That(result, Is.Zero);
         }
 
+        [Test]
         public void WeekdaysBetweenTwoDates_WhenItIsALeapYear_ShouldIncludeFeb29thInResult()
         {
             // Arrange
@@ -68,7 +98,7 @@ namespace DesignCrowdTechnicalChallengeTests
             var result = _sut.WeekdaysBetweenTwoDates(firstDate, secondDate);
 
             // Assert
-            Assert.That(result, Is.EqualTo(3));
+            Assert.That(result, Is.EqualTo(4));
         }
     }
 }
