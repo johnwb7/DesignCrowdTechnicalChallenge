cd6bde703a82432627cb51550349a14fa9b63244
jb <jwburkitt7@gmail.com>
2023-10-15 19:19:54 +1100

    Added skeleton project with scaffold class from exercise sheet. Added empty test project. Added gitignore and empty readme

---
+++ DesignCrowdTechnicalChallenge/BusinessDayCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignCrowdTechnicalChallenge
+{
+    public class BusinessDayCounter
+    {
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            //todo
+        }
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime>
+        publicHolidays)
+        {
+            //todo
+        }
+    }
+}
